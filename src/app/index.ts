752ad8f1de5facdc5eed80ae55585be6d1a4253a
Vitaly Pankov <v.pankov>
2020-06-14 01:05:26 +0300

    added player movement

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,20 @@
-alert('sdfs');
+// this is where everything starts
+
+import {Game} from "./Game"
+
+var game = new Game()
+
+//game.handleCollisions.bind(game)
+window.addEventListener("keydown", (game.onKeyDown.bind(game)))
+window.addEventListener("keyup", (game.onKeyUp.bind(game)))
+
+
+function gameLoop() {
+  requestAnimationFrame(gameLoop)
+  // Drawing code goes here
+  game.update()
+  game.draw()
+}
+gameLoop()
 
 require('./css/app.css')
